861e16972f9833a4716008cfe5c6b2c5ce6735c8
Fernando <faguiar408@gmail.com>
2023-04-17 20:14:11 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+
+import http from 'http';
+//import cors from 'cors';
+import express from "express";
+import bodyParser from 'body-parser';
+
+import routes from './routes';
+import database from './database/database';
+
+class Application {
+    server: http.Server;
+    express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.server = http.createServer(this.express);
+
+        this._setMiddlewares();
+        this._setRoutes();
+
+        console.log(database);
+        
+    }
+
+    private _setMiddlewares(): void {
+        // this.express.use(cors({
+        //     origin: ['http://localhost:4200'],
+        // }));
+        this.express.use(express.json());
+        this.express.use(bodyParser.urlencoded({ extended: true }));
+    }
+
+    private _setRoutes(): void {
+        this.express.use(routes);
+    }
+
+}
+
+export default new Application().server;
