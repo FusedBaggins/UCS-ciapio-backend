6c40329180a1b65d009368d752e15a7620253f38
Fernando <faguiar408@gmail.com>
2023-04-21 20:49:41 -0300

    ajuste nas rotas cadastradas

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import perguntaController from "./entidades/pergunta/pergunta.controller";
 import processoController from "./entidades/processo/processo.controller";
 import instituicaoController from "./entidades/instituicao/instituicao.controller";
 import unidadeFederativaController from './entidades/unidade-federativa/unidade-federativa.controller';
+import respostaController from "./entidades/resposta/resposta.controller";
 
 const routes = Router();
 
@@ -25,7 +26,9 @@ routes.get('/vara/', varaController.list);
 routes.get('/vara/:id', varaController.detail);
 routes.get('/pergunta/', perguntaController.list);
 routes.get('/pergunta/:id', perguntaController.detail);
-routes.get('/pergunta/', processoController.list);
-routes.get('/pergunta/:id', processoController.detail);
+routes.get('/resposta/', respostaController.list);
+routes.get('/resposta/:id', respostaController.detail);
+routes.get('/processo/', processoController.list);
+routes.get('/processo/:id', processoController.detail);
 
 export default routes;
