8bf01e50f951098b23367db61af0e7e59b25d057
Fernando <faguiar408@gmail.com>
2023-05-08 21:13:25 -0300

    adicionado novos itens no menu

--- acl-rules.ts
+++ acl-rules.ts
@@ -1,53 +1,67 @@
 const aclRules = [
-    {
-      group: "usuarioCIAP",
-      permissions: [
-        {
-          resource: "/processo/*",
-          methods: ["POST", "GET", "PUT"],
-          action: "allow",
-          name: "Processos",
-          visible: true,
-        },
-        {
-            resource: "/menu/*",
-            methods: ["POST", "GET", "PUT"],
-            action: "allow",
-            name: "Menus",
-            visible: false,
-        },
-      ]
-    },
-    {
-      group: "usuarioEntidade",
-      permissions: [
-        {
-          resource: "/menu/*",
-          methods: ["POST", "GET", "PUT"],
-          action: "allow",
-          name: "Menus",
-          visible: false,
-        },
-      ]
-    },
-    {
-      group: "deslogado",
-      permissions: [
-        {
-          resource: "/login",
-          methods: "*",
-          action: "allow",
-          name: "Login",
-          visible: false,
-        },
-        {
-          resource: "/usuario/",
-          methods:  ["POST"],
-          action: "allow",
-          name: "Criar usuário",
-          visible: false,
-        },
-      ],
-    }
+  {
+    group: "usuarioCIAP",
+    permissions: [
+      {
+        resource: "/menu/*",
+        methods: ["POST", "GET", "PUT"],
+        action: "allow",
+        name: "Menus",
+        visible: false,
+      },
+      {
+        resource: "/processo/*",
+        methods: ["POST", "GET", "PUT"],
+        action: "allow",
+        name: "Processos",
+        visible: false,
+      },
+      {
+        resource: "/ciap/*",
+        methods: ["POST", "GET", "PUT"],
+        action: "allow",
+        name: "CIAP",
+        visible: true,
+      },
+      {
+        resource: "/prestador/*",
+        methods: ["POST", "GET", "PUT"],
+        action: "allow",
+        name: "Prestador",
+        visible: true,
+      } 
+    ]
+  },
+  {
+    group: "usuarioEntidade",
+    permissions: [
+      {
+        resource: "/menu/*",
+        methods: ["POST", "GET", "PUT"],
+        action: "allow",
+        name: "Menus",
+        visible: false,
+      },
+    ]
+  },
+  {
+    group: "deslogado",
+    permissions: [
+      {
+        resource: "/login",
+        methods: "*",
+        action: "allow",
+        name: "Login",
+        visible: false,
+      },
+      {
+        resource: "/usuario/",
+        methods: ["POST"],
+        action: "allow",
+        name: "Criar usuário",
+        visible: false,
+      },
+    ],
+  }
 ];
 export default aclRules;
